af2b8b886b07cf5fbda3ad29c79e87d7e16d5fee
jyzzyj <11811759+jyzzyj@user.noreply.gitee.com>
2024-03-31 20:22:18 +0800

    1

--- pyzyj/cal_tgt_size_heatmap.py
+++ pyzyj/cal_tgt_size_heatmap.py
@@ -59,7 +59,7 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
                     bh = int(box[3] * h)
                     hw_pair.append((bh, bw))
         else:
-            raise ValueError(f'box_format {box_format} not supported')
+            raise ValueError(f'box_format {box_format} not supported, only support hbb and obb')
         return sizes, size_cnt_map, hw_pair
 
     has_train = 'train' in os.listdir(f'{yolo_root}/images') and 'train' in os.listdir(f'{yolo_root}/labels')
@@ -71,31 +71,31 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
         img_root = fr'{yolo_root}/images/{s}'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/{s}'
-        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root)
+        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
     elif not has_train and not has_val:
         img_root = fr'{yolo_root}/images/test'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/test'
-        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root)
+        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
     elif not has_test and not has_val:
         img_root = fr'{yolo_root}/images/train'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/train'
-        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root)
+        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
     elif not has_test and not has_train:
         img_root = fr'{yolo_root}/images/val'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/val'
-        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root)
+        sizes, size_cnt_map, hw_pair = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
     elif not has_val:
         img_root = fr'{yolo_root}/images/train'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/train'
-        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root)
+        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         img_root = fr'{yolo_root}/images/test'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/test'
-        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root)
+        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         sizes = sizes_train + sizes_test
         size_cnt_map = size_cnt_map_train
         for k, v in size_cnt_map_test.items():
@@ -107,11 +107,11 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
         img_root = fr'{yolo_root}/images/train'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/train'
-        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root)
+        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         img_root = fr'{yolo_root}/images/val'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/val'
-        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root)
+        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         sizes = sizes_train + sizes_val
         size_cnt_map = size_cnt_map_train
         for k, v in size_cnt_map_val.items():
@@ -123,11 +123,11 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
         img_root = fr'{yolo_root}/images/test'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/test'
-        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root)
+        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         img_root = fr'{yolo_root}/images/val'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/val'
-        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root)
+        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         sizes = sizes_test + sizes_val
         size_cnt_map = size_cnt_map_test
         for k, v in size_cnt_map_val.items():
@@ -139,15 +139,15 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
         img_root = fr'{yolo_root}/images/train'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/train'
-        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root)
+        sizes_train, size_cnt_map_train, hw_pair_train = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         img_root = fr'{yolo_root}/images/test'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/test'
-        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root)
+        sizes_test, size_cnt_map_test, hw_pair_test = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         img_root = fr'{yolo_root}/images/val'
         imgs = os.listdir(img_root)
         lbl_root = fr'{yolo_root}/labels/val'
-        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root)
+        sizes_val, size_cnt_map_val, hw_pair_val = __get_sizes(imgs, img_root, lbl_root, box_format=box_format)
         sizes = sizes_train + sizes_test + sizes_val
         size_cnt_map = size_cnt_map_train
         for k, v in size_cnt_map_test.items():
@@ -164,7 +164,7 @@ def yolo_cal_tgt_size_dist(yolo_root=r'/data/ll/code/sod/YOLOv8_With_ODConv/data
 
 
 
-def cal_tgt_size_dist(*,res='c', yolo_root=None, coco_root=None, format='yolo',s=None):
+def cal_tgt_size_dist(*,res='c', yolo_root=None, coco_root=None, format='yolo',s=None, box_format='hbb'):
     """
     :param res: If res is set to 'c', return the size_cnt_map,
     else return the sizes list, size cnt map, hieght width pair
@@ -174,7 +174,7 @@ def cal_tgt_size_dist(*,res='c', yolo_root=None, coco_root=None, format='yolo',s
     :return:
     """
     if format == 'yolo':
-        sizes, size_cnt_map, hw_pair = yolo_cal_tgt_size_dist(yolo_root,s)
+        sizes, size_cnt_map, hw_pair = yolo_cal_tgt_size_dist(yolo_root,s, box_format=box_format)
     elif format == 'coco':
         raise NotImplementedError
     if res == 'c':
@@ -183,7 +183,7 @@ def cal_tgt_size_dist(*,res='c', yolo_root=None, coco_root=None, format='yolo',s
 
 
 def gen_heatmap(*, res='a', base=None,yolo_root=None, coco_root=None,
-                filename='heatmap.png', threshold: Union[list, tuple, int, float] = None,s=None):
+                filename='heatmap.png', threshold: Union[list, tuple, int, float] = None,s=None, box_format='hbb'):
     """
     :param base: If base is not None, the width and height will be rounded
     to the maximum multiple of base less than the original width and height
@@ -198,9 +198,9 @@ def gen_heatmap(*, res='a', base=None,yolo_root=None, coco_root=None,
     if yolo_root is not None and coco_root is not None:
         raise ValueError('yolo_root and coco_root cannot be both not None')
     if yolo_root is not None:
-        _, _, hw_pair = cal_tgt_size_dist(res=res,yolo_root=yolo_root, format=format,s=s)
+        _, _, hw_pair = cal_tgt_size_dist(res=res,yolo_root=yolo_root, format=format,s=s, box_format=box_format)
     else:
-        _, _, hw_pair = cal_tgt_size_dist(coco_root=coco_root, format=format,s=s)
+        _, _, hw_pair = cal_tgt_size_dist(coco_root=coco_root, format=format,s=s, box_format=box_format)
     # hw_pair = hw_pair[:100]
     h_threshold = 100
     w_threshold = 100
